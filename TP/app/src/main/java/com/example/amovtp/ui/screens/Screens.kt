ad1fecb1d4c5ecadc80fcf1aeda630eba88bc6cf
João Morais <72463113+joaormorais@users.noreply.github.com>
2023-12-06 21:39:52 +0000

    Adicionado um comentário

--- TP/app/src/main/java/com/example/amovtp/ui/screens/Screens.kt
+++ TP/app/src/main/java/com/example/amovtp/ui/screens/Screens.kt
@@ -1,10 +1,12 @@
 package com.example.amovtp.ui.screens
 
+//TODO: transformar este enum numa lista de paths?
+
 enum class Screens(val display: String) {
     LOGIN("Login"),
     REGISTER("Register"),
     LOCATIONS("Locations"),
-    POINTS_OF_INTEREST("PointsOfInterest?itemName={itemName}"),
+    POINTS_OF_INTEREST("PointsOfInterest"),
     ADD_LOCATION("AddLocation"),
     ADD_POINT_OF_INTEREST("AddPointOfInterest"),
     ADD_CATEGORY("AddCategory");
