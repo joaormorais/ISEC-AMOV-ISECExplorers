efb24d1e2a51f03252ac9275703730f9bef15678
João Morais <72463113+joaormorais@users.noreply.github.com>
2023-12-07 19:32:19 +0000

    Alterada variavel para obter o itemName

--- TP/app/src/main/java/com/example/amovtp/ui/screens/infoScreens/PointsOfInterestScreen.kt
+++ TP/app/src/main/java/com/example/amovtp/ui/screens/infoScreens/PointsOfInterestScreen.kt
@@ -15,6 +15,7 @@ import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -45,9 +46,12 @@ fun PointsOfInterestScreen(
         mutableStateOf(pointsOfInterestViewModel.getPointsOfInterest())
     }
 
-    if (itemName != null)
-        if (itemName != stringResource(R.string.defaultvalue))
-            pointsOfInterest = pointsOfInterestViewModel.getPointsFromLocation(itemName)
+    val defaultString = stringResource(R.string.defaultvalue)
+    LaunchedEffect(key1 = itemName){
+        if (itemName != null)
+            if (itemName != defaultString)
+                pointsOfInterest = pointsOfInterestViewModel.getPointsFromLocation(itemName)
+    }
 
     Column(
         modifier = modifier
